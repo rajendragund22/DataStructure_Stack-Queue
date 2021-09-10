6612cb24b6aa5a06148fdfe8b21bebf2e1d5e83d
RajendraGund <rajendragund2015@gmail.com>
2021-09-11 02:10:43 +0530

    Added : Program for the Create the Stack

--- src/main/java/com/bridgelabz/Operation.java
+++ src/main/java/com/bridgelabz/Operation.java
@@ -1,4 +1,16 @@
 package com.bridgelabz;
 
 public class Operation {
+    public static StackQueue addElement() {
+        StackQueue stacklist =new StackQueue();
+        stacklist.push(70);
+        stacklist.push(30);
+        stacklist.push(56);
+
+        stacklist.display();
+        return stacklist;
+
+    }
+
+
 }
